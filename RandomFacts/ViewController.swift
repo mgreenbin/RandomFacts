b5d5188639e9fa5db32bfc7439185d0db7d390f2
Mark Greenberg <magreenberg@gmail.com>
2020-01-26 15:43:02 -0600

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by Mark Greenberg on 1/26/20.
+//  Copyright © 2020 MarkStar Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
