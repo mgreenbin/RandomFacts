b5764b35a2c49c8314bb1785fdb260ee26db9d48
Mark Greenberg <magreenberg@gmail.com>
2020-01-26 19:40:39 -0600

    Added initial GUI elements, random number generator, and facts array.

--- RandomFacts/ViewController.swift
+++ RandomFacts/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var randomFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAnotherFunFact(_ sender: UIButton) {
+        
+        let facts = [ "Ostriches can run faster than horses",
+                      "Ants stretch when they wake up in the morning",
+                      "Olympic gold medals are actually made mostly of silver",
+                      "You are born with 300 bones; by the time you are an adult you will have 206",
+                      "It takes about 8 minutes for the light from the Sun to reach Earth",
+                      "Some bamboo plants can grow almost a meter in just one day",
+                      "The state of Florida is bigger than England",
+                      "Some penguins can leap 2-3 meters out of the water",
+                      "On average, it takes 66 days to form a new habit",
+                      "Mammoths still walked the Earth when the Great Pyramid was being built"
+        ]
+        
+     
+        let randomNumber = Int.random(in: 0 ... facts.count - 1)
+        print("New random number is  \(randomNumber)")
+        
+        randomFactLabel.text = facts[randomNumber]
+    }
+    
 }
 
